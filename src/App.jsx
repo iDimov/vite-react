ab96ffd2f3728948976e70ba9a1a99cb7008f956
valeriiD <yadimov@gmail.com>
2025-09-06 14:24:59 +0300

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,12 @@
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { fetchContacts } from './redux/contactsOps'
-import ContactForm from './components/ContactForm'
-import ContactList from './components/ContactList'
-import SearchBox from './components/SearchBox'
 import './App.css'
 
 function App() {
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    dispatch(fetchContacts())
-  }, [dispatch])
-
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Contact Book</h1>
+        <h1>Welcome to React</h1>
+        <p>Start building your awesome app!</p>
       </header>
-      
-      <div className="content-wrapper">
-        <div className="left-column">
-          <ContactForm />
-        </div>
-        
-        <div className="right-column">
-          <SearchBox />
-          
-          <div className="contacts-section">
-            <ContactList />
-          </div>
-        </div>
-      </div>
     </div>
   )
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,7 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { contactsReducer } from './contactsSlice';
-import { filtersReducer } from './filtersSlice';
+import { configureStore } from '@reduxjs/toolkit'
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
-    filters: filtersReducer
-  }
-});
+    // Add your reducers here
+  },
+})
